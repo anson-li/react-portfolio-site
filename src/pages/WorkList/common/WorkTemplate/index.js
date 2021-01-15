b307dfe18663b95671579f027bc6b840f12c9487
Anson Li <hello@ansonli.ca>
2021-01-14 22:59:30 -0700

    Refactored out worklist to use components instead

---
+++ src/pages/WorkList/common/WorkTemplate/index.js
@@ -0,0 +1,77 @@
+import React, { PureComponent } from 'react';
+import WOW from 'wowjs';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+import LineBreak from '../../../../web/assets/line-break.png';
+
+class WorkTemplate extends PureComponent {
+  componentDidMount() {
+    new WOW.WOW({
+      boxClass: 'wow',
+      animateClass: 'animated',
+      offset: 0,
+      mobile: true,
+      live: false,
+    }).init();
+  }
+
+  render() {
+    const {
+      title, description, panel, link, lineBreak,
+    } = this.props;
+    return (
+      <div className="wow fadeIn">
+        <h3>{title}</h3>
+        <br />
+
+        <div className="col-md-12 no-padding">
+          <div className="col-md-6 no-padding">
+            <p>
+              {description}
+            </p>
+            <div className="spacer-xs" />
+          </div>
+        </div>
+        <img className="img-panel" src={panel} alt={`${title} Panel`} />
+        <div className="col-md-12 no-padding">
+          <div className="col-md-6 no-padding">
+            <div className="spacer-xs" />
+            <p>
+              <Link to={link} href={link} className="btn">View case study</Link>
+            </p>
+            <div className="spacer-xs" />
+          </div>
+        </div>
+        <div className="col-md-12 no-padding">
+          <div className="visible-xs">
+            <div className="spacer-xs" />
+          </div>
+          { lineBreak && (
+            <div className="center-sm paddingtop-0">
+              <img className="line-break" src={LineBreak} alt="Line break" />
+            </div>
+          )}
+          <div className="spacer-xs" />
+          <div className="visible-xs">
+            <div className="spacer-xs" />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+WorkTemplate.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  panel: PropTypes.element.isRequired,
+  link: PropTypes.string.isRequired,
+  lineBreak: PropTypes.bool,
+};
+
+WorkTemplate.defaultProps = {
+  lineBreak: false,
+};
+
+export default WorkTemplate;
--- src/pages/WorkList/index.js
+++ src/pages/WorkList/index.js
@@ -14,6 +14,7 @@ import SSCTEPanel from '../../web/assets/panel/panel-sscte.png';
 import TeamAquaPanel from '../../web/assets/panel/panel-teamaqua.png';
 
 import withTransition from '../../common/WithTransition';
+import WorkTemplate from './common/WorkTemplate';
 
 class WorkList extends PureComponent {
   componentDidMount() {
@@ -68,237 +69,67 @@ class WorkList extends PureComponent {
                 <br />
 
                 <div className="col-md-12">
-
-                  <div className="wow fadeIn">
-                    <h3>Questionmark</h3>
-                    <br />
-
-                    <div className="col-md-12 no-padding">
-                      <div className="col-md-6 no-padding">
-                        <p>
-                          Questionmark creates assessment technologies.
-                          These are tools that primarily deal with building technologies
-                          that involve assessments through their lifecycle - from
-                          creation to assessing to results. I was signed on to build a
-                          key component of their technology - their Learning Tools
-                          Interoperability tool - as well as aid in developing their
-                          main software in Drupal.
-                        </p>
-                        <div className="spacer-xs" />
-                      </div>
-                    </div>
-                    <img className="img-panel" src={QuestionmarkPanel} alt="Questionmark Panel" />
-                    <div className="col-md-12 no-padding">
-                      <div className="col-md-6 no-padding">
-                        <div className="spacer-xs" />
-                        <p>
-                          <Link to="/questionmark" href="/questionmark" className="btn">View case study</Link>
-                        </p>
-                        <div className="spacer-xs" />
-                      </div>
-                    </div>
-                    <div className="col-md-12 no-padding">
-                      <div className="visible-xs">
-                        <div className="spacer-xs" />
-                      </div>
-                      <div className="center-sm paddingtop-0">
-                        <img className="line-break" src={LineBreak} alt="Line break" />
-                      </div>
-                      <div className="spacer-xs" />
-                      <div className="visible-xs">
-                        <div className="spacer-xs" />
-                      </div>
-                    </div>
-                  </div>
-
-                  <div className="wow fadeIn">
-                    <h3>Crude Monitor</h3>
-                    <br />
-
-                    <div className="col-md-12 no-padding">
-                      <div className="col-md-6 no-padding">
-                        <p>
-                          CQI is a Canadian-based company which specialises in monitoring
-                          the quality of North American crude oil. They had tasked me
-                          with upgrading their primary application, Crude Monitor,
-                          to fit the needs and demands of today&rsquo;s generation.
-                        </p>
-                        <div className="spacer-xs" />
-                      </div>
-                    </div>
-                    <img className="img-panel" src={CrudeMonitorPanel} alt="Crude Monitor Panel" />
-                    <div className="col-md-12 no-padding">
-                      <div className="col-md-6 no-padding">
-                        <div className="spacer-xs" />
-                        <p>
-                          <Link to="/crudemonitor" href="/crudemonitor" className="btn">View case study</Link>
-                        </p>
-                        <div className="spacer-xs" />
-                      </div>
-                    </div>
-                    <div className="col-md-12 no-padding">
-                      <div className="visible-xs">
-                        <div className="spacer-xs" />
-                      </div>
-                      <div className="center-sm paddingtop-0">
-                        <img className="line-break" src={LineBreak} alt="Line break" />
-                      </div>
-                      <div className="spacer-xs" />
-                      <div className="visible-xs">
-                        <div className="spacer-xs" />
-                      </div>
-                    </div>
-                  </div>
-
-                  <div className="wow fadeIn">
-                    <h3>AlbertaMES</h3>
-                    <br />
-                    <div className="col-md-12 no-padding">
-                      <div className="col-md-6 no-padding">
-                        <p>
-                          AlbertaMES is a heuristics-based web application to filter
-                          and isolate projects, models and stations associated with
-                          Alberta Environment and Parks. I was signed on as the
-                          primary developer of the application, which was developed
-                          over the course of 8 months.
-                        </p>
-                        <div className="spacer-xs" />
-                      </div>
-                    </div>
-                    <img className="img-panel" src={AlbertaMESPanel} alt="AlbertaMES Panel" />
-                    <div className="col-md-12 no-padding">
-                      <div className="col-md-6 no-padding">
-                        <div className="spacer-xs" />
-                        <p>
-                          <Link to="/albertames" href="/albertames" className="btn">View case study</Link>
-                        </p>
-                        <div className="spacer-xs" />
-                      </div>
-                    </div>
-                    <div className="col-md-12 no-padding">
-                      <div className="visible-xs">
-                        <div className="spacer-xs" />
-                      </div>
-                      <div className="center-sm paddingtop-0">
-                        <img className="line-break" src={LineBreak} alt="Line break" />
-                      </div>
-                      <div className="spacer-xs" />
-                      <div className="visible-xs">
-                        <div className="spacer-xs" />
-                      </div>
-                    </div>
-                  </div>
-
-                  <div className="wow fadeIn">
-                    <h3>#DailyUI</h3>
-                    <br />
-                    <div className="col-md-12 no-padding">
-                      <div className="col-md-6 no-padding">
-                        <p>
-                          DailyUI is, as the name suggests, a daily exploration into UX
-                          and UI. For 100 days, UI and UX elements were designed and
-                          created for the creative community.
-                        </p>
-                        <div className="spacer-xs" />
-                      </div>
-                    </div>
-                    <img className="img-panel" src={DailyUIPanel} alt="DailyUI Panel" />
-                    <div className="col-md-12 no-padding">
-                      <div className="col-md-6 no-padding">
-                        <div className="spacer-xs" />
-                        <p>
-                          <Link to="/dailyui" href="/dailyui" className="btn">View case study</Link>
-                        </p>
-                        <div className="spacer-xs" />
-                      </div>
-                    </div>
-                    <div className="col-md-12 no-padding">
-                      <div className="visible-xs">
-                        <div className="spacer-xs" />
-                      </div>
-                      <div className="center-sm paddingtop-0">
-                        <img className="line-break" src={LineBreak} alt="Line break" />
-                      </div>
-                      <div className="spacer-xs" />
-                      <div className="visible-xs">
-                        <div className="spacer-xs" />
-                      </div>
-                    </div>
-                  </div>
-
-                  <div className="wow fadeIn">
-                    <h3>SSCTE</h3>
-                    <br />
-                    <div className="col-md-12 no-padding">
-                      <div className="col-md-6 no-padding">
-                        <p>
-                          SSCTE is an android application that&rsquo;s designed for collecting and
-                          trading cards. It primarily served as an exploration of common UX
-                          and UI design behavior for Android technologies.
-                        </p>
-                        <div className="spacer-xs" />
-                      </div>
-                    </div>
-                    <img className="img-panel" src={SSCTEPanel} alt="SSCTE Panel" />
-                    <div className="col-md-12 no-padding">
-                      <div className="col-md-6 no-padding">
-                        <div className="spacer-xs" />
-                        <p>
-                          <Link to="/sscte" href="/sscte" className="btn">View case study</Link>
-                        </p>
-                        <div className="spacer-xs" />
-                      </div>
-                    </div>
-                    <div className="col-md-12 no-padding">
-                      <div className="visible-xs">
-                        <div className="spacer-xs" />
-                      </div>
-                      <div className="center-sm paddingtop-0">
-                        <img className="line-break" src={LineBreak} alt="Line break" />
-                      </div>
-                      <div className="spacer-xs" />
-                      <div className="visible-xs">
-                        <div className="spacer-xs" />
-                      </div>
-                    </div>
-                  </div>
-
-                  <div className="wow fadeIn">
-                    <h3>Team Aqua</h3>
-                    <br />
-                    <div className="col-md-12 no-padding">
-                      <div className="col-md-6 no-padding">
-                        <p>
-                          Team Aqua is a development firm, created with the
-                          purpose of exploring Ruby development and design.
-                          Projects developed under this team include monitoring
-                          systems, libraries, and UI.
-                        </p>
-                        <div className="spacer-xs" />
-                        <br />
-                      </div>
-                    </div>
-                    <img className="img-panel" src={TeamAquaPanel} alt="Team Aqua Panel" />
-                    <div className="col-md-12 no-padding">
-                      <div className="col-md-6 no-padding">
-                        <div className="spacer-xs" />
-                        <p>
-                          <Link to="/teamaqua" href="/teamaqua" className="btn">View case study</Link>
-                        </p>
-                        <div className="spacer-xs" />
-                      </div>
-                    </div>
-                    <div className="col-md-12 no-padding">
-                      <div className="visible-xs">
-                        <div className="spacer-xs" />
-                      </div>
-                      <div className="spacer-xs" />
-                      <div className="visible-xs">
-                        <div className="spacer-xs" />
-                      </div>
-                    </div>
-                  </div>
-
+                  <WorkTemplate
+                    title="Questionmark"
+                    description="Questionmark creates assessment technologies.
+                      These are tools that primarily deal with building technologies
+                      that involve assessments through their lifecycle - from
+                      creation to assessing to results. I was signed on to build a
+                      key component of their technology - their Learning Tools
+                      Interoperability tool - as well as aid in developing their
+                      main software in Drupal."
+                    panel={QuestionmarkPanel}
+                    link="/questionmark"
+                    lineBreak
+                  />
+                  <WorkTemplate
+                    title="Crude Monitor"
+                    description="CQI is a Canadian-based company which specialises in monitoring
+                      the quality of North American crude oil. They had tasked me
+                      with upgrading their primary application, Crude Monitor,
+                      to fit the needs and demands of today&rsquo;s generation."
+                    panel={CrudeMonitorPanel}
+                    link="/crudemonitor"
+                    lineBreak
+                  />
+                  <WorkTemplate
+                    title="AlbertaMES"
+                    description="AlbertaMES is a heuristics-based web application to filter
+                      and isolate projects, models and stations associated with
+                      Alberta Environment and Parks. I was signed on as the
+                      primary developer of the application, which was developed
+                      over the course of 8 months."
+                    panel={AlbertaMESPanel}
+                    link="/albertames"
+                    lineBreak
+                  />
+                  <WorkTemplate
+                    title="#DailyUI"
+                    description="DailyUI is, as the name suggests, a daily exploration into UX
+                      and UI. For 100 days, UI and UX elements were designed and
+                      created for the creative community."
+                    panel={DailyUIPanel}
+                    link="/dailyui"
+                    lineBreak
+                  />
+                  <WorkTemplate
+                    title="SSCTE"
+                    description="SSCTE is an android application that&rsquo;s designed for collecting and
+                      trading cards. It primarily served as an exploration of common UX
+                      and UI design behavior for Android technologies."
+                    panel={SSCTEPanel}
+                    link="/sscte"
+                    lineBreak
+                  />
+                  <WorkTemplate
+                    title="Team Aqua"
+                    description="Team Aqua is a development firm, created with the
+                      purpose of exploring Ruby development and design.
+                      Projects developed under this team include monitoring
+                      systems, libraries, and UI."
+                    panel={TeamAquaPanel}
+                    link="/teamaqua"
+                  />
                 </div>
               </div>
               <div className="spacer-sm" />
